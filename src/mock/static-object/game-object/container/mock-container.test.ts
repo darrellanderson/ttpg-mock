97ac8a3a54e8a57af6c62aebd5075c267069ad2d
Darrell <darrell.anderson@gmail.com>
2023-11-24 12:39:27 -0800

    mock-container

--- src/mock/static-object/game-object/container/mock-container.test.ts
+++ src/mock/static-object/game-object/container/mock-container.test.ts
@@ -1,5 +1,179 @@
-import { MockContainer } from "./mock-container";
+import { MockGameObject } from "../mock-game-object";
+import { MockContainer, MockContainerParams } from "./mock-container";
 
 it("constructor", () => {
-  new MockContainer();
+  const params: MockContainerParams = {
+    containerTags: ["my-container-tag"],
+    items: [new MockGameObject()],
+    maxItems: 97,
+    type: 3,
+  };
+  const container = new MockContainer(params);
+  expect(container.getContainerTags()).toEqual(params.containerTags);
+  expect(container.getItems()).toEqual(params.items);
+  expect(container.getMaxItems()).toEqual(params.maxItems);
+  expect(container.getNumItems()).toEqual(params.items?.length);
+  expect(container.getType()).toEqual(params.type);
+});
+
+it("clear", () => {
+  const container = new MockContainer({
+    items: [new MockGameObject()],
+  });
+  expect(container.getNumItems()).toEqual(1);
+  container.clear();
+  expect(container.getNumItems()).toEqual(0);
+});
+
+it("containerTags", () => {
+  const input = ["a", "b"];
+  const container = new MockContainer();
+  container.setContainerTags(input);
+  const output = container.getContainerTags();
+  expect(output).toEqual(input);
+});
+
+it("maxItems", () => {
+  const input = 7;
+  const container = new MockContainer();
+  container.setMaxItems(input);
+  const output = container.getMaxItems();
+  expect(output).toEqual(input);
+});
+
+it("type", () => {
+  const input = 7;
+  const container = new MockContainer();
+  container.setType(input);
+  const output = container.getType();
+  expect(output).toEqual(input);
+});
+
+it("contains", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1],
+  });
+  expect(container.contains(obj1)).toEqual(true);
+  expect(container.contains(obj2)).toEqual(false);
+});
+
+it("getNextTakeIndex", () => {
+  const container = new MockContainer({
+    items: [new MockGameObject()],
+  });
+  let index = container.getNextTakeIndex();
+  expect(index).toEqual(0);
+  container.destroy();
+  index = container.getNextTakeIndex();
+  expect(index).toEqual(-1);
+});
+
+it("addObjects", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2]);
+
+  const obj3 = new MockGameObject();
+  container.addObjects([obj3], 1);
+  expect(container.getItems()).toEqual([obj1, obj3, obj2]);
+
+  const obj4 = new MockGameObject();
+  container.addObjects([obj4]);
+  expect(container.getItems()).toEqual([obj4, obj1, obj3, obj2]);
+});
+
+it("insert", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2]);
+  const obj3 = new MockGameObject();
+  container.insert([obj3], 1);
+  expect(container.getItems()).toEqual([obj1, obj3, obj2]);
+});
+
+it("remove", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const obj3 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2, obj3],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
+  let success = container.remove(obj2);
+  expect(success).toEqual(true);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+  success = container.remove(obj2);
+  expect(success).toEqual(false);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+});
+
+it("removeAt", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const obj3 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2, obj3],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
+  let success = container.removeAt(1);
+  expect(success).toEqual(true);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+  success = container.removeAt(99);
+  expect(success).toEqual(false);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+});
+
+it("take", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const obj3 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2, obj3],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
+  let success = container.take(obj2, [0, 0, 0]);
+  expect(success).toEqual(true);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+  success = container.take(obj2, [0, 0, 0]);
+  expect(success).toEqual(false);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+});
+
+it("takeAt", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const obj3 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2, obj3],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
+  let success = container.takeAt(1, [0, 0, 0]);
+  expect(success).toEqual(obj2);
+  expect(container.getItems()).toEqual([obj1, obj3]);
+  success = container.takeAt(99, [0, 0, 0]);
+  expect(success).toBeUndefined();
+  expect(container.getItems()).toEqual([obj1, obj3]);
+});
+
+it("takeAt (keep)", () => {
+  const obj1 = new MockGameObject();
+  const obj2 = new MockGameObject();
+  const obj3 = new MockGameObject();
+  const container = new MockContainer({
+    items: [obj1, obj2, obj3],
+  });
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
+  const showAnimation = undefined;
+  const keep = true;
+  let success = container.takeAt(1, [0, 0, 0], showAnimation, keep);
+  expect(success).toEqual(obj2);
+  expect(container.getItems()).toEqual([obj1, obj2, obj3]);
 });
--- src/mock/static-object/game-object/container/mock-container.ts
+++ src/mock/static-object/game-object/container/mock-container.ts
@@ -1,20 +1,26 @@
 import {
-  Color,
   Container,
-  DrawingLine,
   GameObject,
   MulticastDelegate,
   Player,
-  Rotator,
-  SnapPoint,
-  Switcher,
-  UIElement,
   Vector,
 } from "@tabletop-playground/api";
 import { MockMulticastDelegate } from "../../../multicast-delegate/mock-multicast-delegate";
-import { MockGameObject } from "../mock-game-object";
+import { MockGameObject, MockGameObjectParams } from "../mock-game-object";
+
+export type MockContainerParams = MockGameObjectParams & {
+  containerTags?: string[];
+  items?: GameObject[];
+  maxItems?: number;
+  type?: number;
+};
 
 export class MockContainer extends MockGameObject implements Container {
+  private _containerTags: string[] = [];
+  private _items: GameObject[] = [];
+  private _maxItems: number = 100;
+  private _type: number = 0;
+
   onInserted: MulticastDelegate<
     (container: this, insertedObjects: GameObject[], player: Player) => void
   > = new MockMulticastDelegate<
@@ -26,73 +32,125 @@ export class MockContainer extends MockGameObject implements Container {
     (container: this, removedObject: GameObject, player: Player) => void
   >();
 
-  takeAt(
-    index: number,
-    position: Vector | [x: number, y: number, z: number],
-    showAnimation?: boolean | undefined,
-    keep?: boolean | undefined
-  ): GameObject | undefined {
-    throw new Error("Method not implemented.");
+  constructor(params?: MockContainerParams) {
+    super(params);
+    if (params?.containerTags) {
+      this._containerTags = params.containerTags;
+    }
+    if (params?.items) {
+      this._items = params.items;
+    }
+    if (params?.maxItems !== undefined) {
+      this._maxItems = params.maxItems;
+    }
+    if (params?.type !== undefined) {
+      this._type = params.type;
+    }
   }
+
+  addObjects(
+    objects: GameObject[],
+    index?: number | undefined,
+    showAnimation?: boolean | undefined
+  ): void {
+    index = index ? index : 0;
+    this._items = [
+      ...this._items.slice(0, index),
+      ...objects,
+      ...this._items.slice(index),
+    ];
+  }
+
+  clear(): void {
+    this._items = [];
+  }
+
+  contains(checkObject: GameObject): boolean {
+    return this._items.includes(checkObject);
+  }
+
+  getContainerTags(): string[] {
+    return this._containerTags;
+  }
+
+  getItems(): GameObject[] {
+    return this._items;
+  }
+
+  getMaxItems(): number {
+    return this._maxItems;
+  }
+
+  getNextTakeIndex(): number {
+    if (!this.isValid() || this.getNumItems() === 0) {
+      return -1;
+    }
+    return 0; // random *could* choose another index
+  }
+
+  getNumItems(): number {
+    return this._items.length;
+  }
+
+  getType(): number {
+    return this._type;
+  }
+
+  insert(
+    objects: GameObject[],
+    index?: number | undefined,
+    showAnimation?: boolean | undefined
+  ): void {
+    this.addObjects(objects, index, showAnimation);
+  }
+
+  remove(objectToRemove: GameObject): boolean {
+    const index = this._items.indexOf(objectToRemove);
+    return this.removeAt(index);
+  }
+
+  removeAt(index: number): boolean {
+    if (index < 0 || index >= this._items.length) {
+      return false;
+    }
+    this._items.splice(index, 1);
+    return true;
+  }
+
+  setContainerTags(tags: string[]): void {
+    this._containerTags = tags;
+  }
+
+  setMaxItems(maxItems: number): void {
+    this._maxItems = maxItems;
+  }
+
+  setType(newType: number): void {
+    this._type = newType;
+  }
+
   take(
     objectToRemove: GameObject,
     position: Vector | [x: number, y: number, z: number],
     showAnimation?: boolean | undefined,
     keep?: boolean | undefined
   ): boolean {
-    throw new Error("Method not implemented.");
+    const index = this._items.indexOf(objectToRemove);
+    const obj = this.takeAt(index, position, showAnimation, keep);
+    return obj ? true : false;
   }
-  setType(newType: number): void {
-    throw new Error("Method not implemented.");
-  }
-  setMaxItems(maxItems: number): void {
-    throw new Error("Method not implemented.");
-  }
-  setContainerTags(tags: string[]): void {
-    throw new Error("Method not implemented.");
-  }
-  removeAt(index: number): boolean {
-    throw new Error("Method not implemented.");
-  }
-  remove(objectToRemove: GameObject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  insert(
-    objects: GameObject[],
-    index?: number | undefined,
-    showAnimation?: boolean | undefined
-  ): void {
-    throw new Error("Method not implemented.");
-  }
-  getType(): number {
-    throw new Error("Method not implemented.");
-  }
-  getNumItems(): number {
-    throw new Error("Method not implemented.");
-  }
-  getNextTakeIndex(): number {
-    throw new Error("Method not implemented.");
-  }
-  getMaxItems(): number {
-    throw new Error("Method not implemented.");
-  }
-  getItems(): GameObject[] {
-    throw new Error("Method not implemented.");
-  }
-  getContainerTags(): string[] {
-    throw new Error("Method not implemented.");
-  }
-  contains(checkObject: GameObject): boolean {
-    throw new Error("Method not implemented.");
-  }
-  clear(): void {
-    throw new Error("Method not implemented.");
-  }
-  addObjects(
-    objects: GameObject[],
-    index?: number | undefined,
-    showAnimation?: boolean | undefined
-  ): void {
-    throw new Error("Method not implemented.");
+
+  takeAt(
+    index: number,
+    position: Vector | [x: number, y: number, z: number],
+    showAnimation?: boolean | undefined,
+    keep?: boolean | undefined
+  ): GameObject | undefined {
+    const obj = this._items[index];
+    if (!obj || (!keep && !this.removeAt(index))) {
+      return undefined;
+    }
+    obj.setPosition(position);
+    return obj;
   }
 }
