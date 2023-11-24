4de15c5ed48646a6686c958167160321b734906e
Darrell <darrell.anderson@gmail.com>
2023-11-24 11:30:04 -0800

    card-holder

--- src/mock/static-object/game-object/card-holder/mock-card-holder.test.ts
+++ src/mock/static-object/game-object/card-holder/mock-card-holder.test.ts
@@ -1,5 +1,67 @@
-import { MockCardHolder } from "./mock-card-holder";
+import { HiddenCardsType } from "@tabletop-playground/api";
+import { MockCard } from "../card/mock-card";
+import { MockCardHolder, MockCardHolderParams } from "./mock-card-holder";
 
 it("constructor", () => {
-  new MockCardHolder();
+  const params: MockCardHolderParams = {
+    cards: [new MockCard()],
+    onlyOwnerTakesCards: true,
+    hiddenCardsType: HiddenCardsType.Back,
+  };
+  const holder = new MockCardHolder(params);
+  expect(holder.getCards()).toEqual(params.cards);
+  expect(holder.getNumCards()).toEqual(1);
+  expect(holder.getOnlyOwnerTakesCards()).toEqual(params.onlyOwnerTakesCards);
+  expect(holder.getHiddenCardsType()).toEqual(params.hiddenCardsType);
+});
+
+it("hiddenType", () => {
+  const input = HiddenCardsType.Back;
+  const holder = new MockCardHolder();
+  holder.setHiddenCardsType(input);
+  const output = holder.getHiddenCardsType();
+  expect(output).toEqual(input);
+});
+
+it("onlyOwnerTakesCards", () => {
+  const input = true;
+  const holder = new MockCardHolder();
+  holder.setOnlyOwnerTakesCards(input);
+  const output = holder.getOnlyOwnerTakesCards();
+  expect(output).toEqual(input);
+});
+
+it("holds", () => {
+  const card1 = new MockCard();
+  const card2 = new MockCard();
+  const holder = new MockCardHolder({ cards: [card1] });
+  expect(holder.holds(card1)).toEqual(true);
+  expect(holder.holds(card2)).toEqual(false);
+});
+
+it("insert", () => {
+  const card1 = new MockCard();
+  const card2 = new MockCard();
+  const holder = new MockCardHolder({ cards: [card1] });
+  expect(holder.getCards()).toEqual([card1]);
+  holder.insert(card2, 0);
+  expect(holder.getCards()).toEqual([card2, card1]);
+});
+
+it("moveCard", () => {
+  const card1 = new MockCard();
+  const card2 = new MockCard();
+  const holder = new MockCardHolder({ cards: [card1, card2] });
+  expect(holder.getCards()).toEqual([card1, card2]);
+  holder.moveCard(card2, 0);
+  expect(holder.getCards()).toEqual([card2, card1]);
+});
+
+it("removeAt", () => {
+  const card1 = new MockCard();
+  const card2 = new MockCard();
+  const holder = new MockCardHolder({ cards: [card1, card2] });
+  expect(holder.getCards()).toEqual([card1, card2]);
+  holder.removeAt(0);
+  expect(holder.getCards()).toEqual([card2]);
 });
--- src/mock/static-object/game-object/card-holder/mock-card-holder.ts
+++ src/mock/static-object/game-object/card-holder/mock-card-holder.ts
@@ -6,10 +6,19 @@ import {
 } from "@tabletop-playground/api";
 import { MockGameObject, MockGameObjectParams } from "../mock-game-object";
 import { MockMulticastDelegate } from "../../../multicast-delegate/mock-multicast-delegate";
+import { HiddenCardsType } from "../../../../enums";
 
-export type MockCardHolderParams = MockGameObjectParams & {};
+export type MockCardHolderParams = MockGameObjectParams & {
+  cards?: Card[];
+  onlyOwnerTakesCards?: boolean;
+  hiddenCardsType?: number;
+};
 
 export class MockCardHolder extends MockGameObject implements CardHolder {
+  private _cards: Card[] = [];
+  private _onlyOwnerTakesCards: boolean = false;
+  private _hiddenCardsType: number = HiddenCardsType.GreyBlur;
+
   onInserted: MulticastDelegate<
     (holder: this, insertedCard: Card, player: Player, index: number) => void
   > = new MockMulticastDelegate<
@@ -35,48 +44,79 @@ export class MockCardHolder extends MockGameObject implements CardHolder {
 
   constructor(params?: MockCardHolderParams) {
     super(params);
+    if (params?.cards) {
+      this._cards = params.cards;
+    }
+    if (params?.onlyOwnerTakesCards !== undefined) {
+      this._onlyOwnerTakesCards = params.onlyOwnerTakesCards;
+    }
+    if (params?.hiddenCardsType !== undefined) {
+      this._hiddenCardsType = params.hiddenCardsType;
+    }
+  }
+
+  getCards(): Card[] {
+    return [...this._cards];
+  }
+
+  getHiddenCardsType(): number {
+    return this._hiddenCardsType;
+  }
+
+  getNumCards(): number {
+    return this._cards.length;
+  }
+
+  getOnlyOwnerTakesCards(): boolean {
+    return this._onlyOwnerTakesCards;
+  }
+
+  holds(card: Card): boolean {
+    return this._cards.includes(card);
+  }
+
+  insert(card: Card, index: number): boolean {
+    this._cards = [
+      ...this._cards.slice(0, index),
+      card,
+      ...this._cards.slice(index),
+    ];
+    return true; // editor setting for max limit
+  }
+
+  moveCard(card: Card, index: number): void {
+    const oldIndex = this._cards.indexOf(card);
+    if (oldIndex >= 0) {
+      this.removeAt(oldIndex);
+      this.insert(card, index);
+    }
+  }
+
+  removeAt(index: number): Card | undefined {
+    return this._cards.splice(index, 1)[0];
+  }
+
+  setHiddenCardsType(newType: number): void {
+    this._hiddenCardsType = newType;
   }
 
   setOnlyOwnerTakesCards(onlyOwner: boolean): void {
+    this._onlyOwnerTakesCards = onlyOwner;
+  }
+
+  // ----------------------------------
+
+  flipCard(card: Card): void {
     throw new Error("Method not implemented.");
   }
-  setHiddenCardsType(newType: number): void {
-    throw new Error("Method not implemented.");
-  }
-  rotateCard(card: Card): void {
-    throw new Error("Method not implemented.");
-  }
-  removeAt(index: number): Card | undefined {
-    throw new Error("Method not implemented.");
-  }
-  moveCard(card: Card, index: number): void {
+
+  isCardFaceUp(card: Card): boolean {
     throw new Error("Method not implemented.");
   }
   isCardUpsideDown(card: Card): boolean {
     throw new Error("Method not implemented.");
   }
-  isCardFaceUp(card: Card): boolean {
-    throw new Error("Method not implemented.");
-  }
-  insert(card: Card, index: number): boolean {
-    throw new Error("Method not implemented.");
-  }
-  holds(card: Card): boolean {
-    throw new Error("Method not implemented.");
-  }
-  getOnlyOwnerTakesCards(): boolean {
-    throw new Error("Method not implemented.");
-  }
-  getNumCards(): number {
-    throw new Error("Method not implemented.");
-  }
-  getHiddenCardsType(): number {
-    throw new Error("Method not implemented.");
-  }
-  getCards(): Card[] {
-    throw new Error("Method not implemented.");
-  }
-  flipCard(card: Card): void {
+  rotateCard(card: Card): void {
     throw new Error("Method not implemented.");
   }
 }
