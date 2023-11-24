ff6be039b86c955eb5d96f9034a277665ca09aa8
Darrell <darrell.anderson@gmail.com>
2023-11-24 11:04:31 -0800

    card

--- src/mock/static-object/game-object/card/mock-card.test.ts
+++ src/mock/static-object/game-object/card/mock-card.test.ts
@@ -1,8 +1,128 @@
-import { MockCard } from "./mock-card";
+import { MockCard, MockCardParams } from "./mock-card";
 import { MockCardDetails } from "../../../card-details/mock-card-details";
+import { MockCardHolder } from "../card-holder/mock-card-holder";
+import { MockVector } from "../../../vector/mock-vector";
 
 it("constructor", () => {
-  new MockCard({});
+  const params: MockCardParams = {
+    cardDetails: [new MockCardDetails({})],
+    cardHolder: new MockCardHolder(),
+    isFaceUp: true,
+  };
+  const card = new MockCard(params);
+  expect(card.getAllCardDetails()).toEqual(params.cardDetails);
+  expect(card.getHolder()).toEqual(params.cardHolder);
+  expect(card.isInHolder()).toEqual(true);
+  expect(card.isInHand()).toEqual(false);
+  expect(card.getStackSize()).toEqual(params.cardDetails?.length);
+  expect(card.isFaceUp()).toEqual(params.isFaceUp);
+});
+
+it("addCards (size mismatch)", () => {
+  const src = new MockCard({
+    _modelSize: new MockVector(1, 3, 1),
+    cardDetails: [new MockCardDetails({ name: "src1" })],
+  });
+  const dst = new MockCard({
+    _modelSize: new MockVector(3, 1, 1),
+    cardDetails: [new MockCardDetails({ name: "dst1" })],
+  });
+  expect(dst.canAddCards(src)).toEqual(false);
+  const success = dst.addCards(src);
+  expect(success).toEqual(false);
+});
+
+it("addCards (standard)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const dst = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "dst1" }),
+      new MockCardDetails({ name: "dst2" }),
+      new MockCardDetails({ name: "dst3" }),
+    ],
+  });
+
+  const success = dst.addCards(src);
+  expect(success).toEqual(true);
+  const names = dst.getAllCardDetails().map((details) => details.name);
+  expect(names).toEqual(["dst1", "dst2", "dst3", "src1", "src2", "src3"]);
+});
+
+it("addCards (toFront)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const dst = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "dst1" }),
+      new MockCardDetails({ name: "dst2" }),
+      new MockCardDetails({ name: "dst3" }),
+    ],
+  });
+
+  const toFront = true;
+  const success = dst.addCards(src, toFront);
+  expect(success).toEqual(true);
+  const names = dst.getAllCardDetails().map((details) => details.name);
+  expect(names).toEqual(["src1", "src2", "src3", "dst1", "dst2", "dst3"]);
+});
+
+it("addCards (offset)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const dst = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "dst1" }),
+      new MockCardDetails({ name: "dst2" }),
+      new MockCardDetails({ name: "dst3" }),
+    ],
+  });
+
+  const toFront = false;
+  const offset = 1;
+  const success = dst.addCards(src, toFront, offset);
+  expect(success).toEqual(true);
+  const names = dst.getAllCardDetails().map((details) => details.name);
+  expect(names).toEqual(["dst1", "dst2", "src1", "src2", "src3", "dst3"]);
+});
+
+it("addCards (toFront, offset)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const dst = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "dst1" }),
+      new MockCardDetails({ name: "dst2" }),
+      new MockCardDetails({ name: "dst3" }),
+    ],
+  });
+
+  const toFront = true;
+  const offset = 1;
+  const success = dst.addCards(src, toFront, offset);
+  expect(success).toEqual(true);
+  const names = dst.getAllCardDetails().map((details) => details.name);
+  expect(names).toEqual(["dst1", "src1", "src2", "src3", "dst2", "dst3"]);
 });
 
 it("cardDetails", () => {
@@ -17,4 +137,209 @@ it("cardDetails", () => {
   expect(card.getCardDetails().name).toEqual("a");
   expect(card.getCardDetails(1)?.name).toEqual("b");
   expect(card.getCardDetails(3)).toBeUndefined();
+});
+
+it("divide", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const stacks = src.divide(2);
+  expect(stacks.length).toEqual(2);
+  const [a, b] = stacks;
+  expect(a.getStackSize()).toEqual(2);
+  expect(b.getStackSize()).toEqual(1);
+});
+
+it("moveCardInStack", () => {
+  const card = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  card.moveCardInStack(0, 1);
+  const names = card.getAllCardDetails().map((details) => details.name);
+  expect(names).toEqual(["src2", "src1", "src3"]);
+});
+
+it("removeFromHolder", () => {
+  const card = new MockCard({
+    cardHolder: new MockCardHolder(),
+  });
+  expect(card.isInHolder()).toBe(true);
+  card.removeFromHolder();
+  expect(card.isInHolder()).toBe(false);
+});
+
+it("setCardIndexAt", () => {
+  const card = new MockCard({
+    cardDetails: [new MockCardDetails()],
+  });
+  expect(card.getCardDetails(0)?.index).toEqual(0);
+  card.setCardIndexAt(7, 0);
+  expect(card.getCardDetails(0)?.index).toEqual(7);
+  card.setCardIndexAt(7, 1000); // out of range
+});
+
+it("setTextureOverrideURL", () => {
+  const card = new MockCard({
+    cardDetails: [new MockCardDetails()],
+  });
+  expect(card.getCardDetails(0)?.textureOverrideURL).toEqual("");
+  card.setTextureOverrideURL("foo");
+  expect(card.getCardDetails(0)?.textureOverrideURL).toEqual("foo");
+});
+
+it("setTextureOverrideURLAt", () => {
+  const card = new MockCard({
+    cardDetails: [new MockCardDetails(), new MockCardDetails()],
+  });
+  expect(card.getCardDetails(0)?.textureOverrideURL).toEqual("");
+  expect(card.getCardDetails(1)?.textureOverrideURL).toEqual("");
+  card.setTextureOverrideURLAt("foo", 0);
+  expect(card.getCardDetails(0)?.textureOverrideURL).toEqual("foo");
+  expect(card.getCardDetails(1)?.textureOverrideURL).toEqual("");
+  card.setTextureOverrideURLAt("foo", 1000); // out of range
+});
+
+it("shuffle", () => {
+  const card = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+    ],
+  });
+  card.shuffle();
+  expect(card.getStackSize()).toEqual(2);
+});
+
+it("split", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const stacks = src.split(2);
+  expect(stacks.length).toEqual(2);
+  const [a, b] = stacks;
+  expect(a.getStackSize()).toEqual(2);
+  expect(b.getStackSize()).toEqual(1);
+});
+
+it("takeCard (standard)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const dst = src.takeCards();
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src1", "src2"]);
+  expect(dstNames).toEqual(["src3"]);
+});
+
+it("takeCard (numCards)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const numCards = 2;
+  const dst = src.takeCards(numCards);
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src1"]);
+  expect(dstNames).toEqual(["src2", "src3"]);
+});
+
+it("takeCard (fromFront)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const numCards = undefined;
+  const fromFront = true;
+  const dst = src.takeCards(numCards, fromFront);
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src2", "src3"]);
+  expect(dstNames).toEqual(["src1"]);
+});
+
+it("takeCard (offset)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+      new MockCardDetails({ name: "src4" }),
+    ],
+  });
+  const numCards = undefined;
+  const fromFront = undefined;
+  const offset = 1;
+  const dst = src.takeCards(numCards, fromFront, offset);
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src1", "src2", "src4"]);
+  expect(dstNames).toEqual(["src3"]);
+});
+
+it("takeCard (fromFront, offset)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+      new MockCardDetails({ name: "src4" }),
+    ],
+  });
+  const numCards = undefined;
+  const fromFront = true;
+  const offset = 1;
+  const dst = src.takeCards(numCards, fromFront, offset);
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src1", "src3", "src4"]);
+  expect(dstNames).toEqual(["src2"]);
+});
+
+it("takeCard (keep)", () => {
+  const src = new MockCard({
+    cardDetails: [
+      new MockCardDetails({ name: "src1" }),
+      new MockCardDetails({ name: "src2" }),
+      new MockCardDetails({ name: "src3" }),
+    ],
+  });
+  const numCards = undefined;
+  const fromFront = undefined;
+  const offset = undefined;
+  const keep = true;
+  const dst = src.takeCards(numCards, fromFront, offset, true);
+  expect(dst).toBeInstanceOf(MockCard);
+  const srcNames = src.getAllCardDetails().map((details) => details.name);
+  const dstNames = dst?.getAllCardDetails().map((details) => details.name);
+  expect(srcNames).toEqual(["src1", "src2", "src3"]);
+  expect(dstNames).toEqual(["src3"]);
 });
--- src/mock/static-object/game-object/card/mock-card.ts
+++ src/mock/static-object/game-object/card/mock-card.ts
@@ -7,20 +7,18 @@ import {
 } from "@tabletop-playground/api";
 import { MockGameObject, MockGameObjectParams } from "../mock-game-object";
 import { MockMulticastDelegate } from "../../../multicast-delegate/mock-multicast-delegate";
+import { MockCardDetails } from "../../../card-details/mock-card-details";
 
 export type MockCardParams = MockGameObjectParams & {
   cardDetails?: CardDetails[];
+  cardHolder?: CardHolder;
+  isFaceUp?: boolean;
 };
 
 export class MockCard extends MockGameObject implements Card {
-  private readonly _cardDetails: CardDetails[] = [];
-
-  constructor(params?: MockCardParams) {
-    super(params);
-    if (params?.cardDetails) {
-      this._cardDetails.push(...params.cardDetails);
-    }
-  }
+  private _cardDetails: CardDetails[] = [];
+  private _cardHolder: CardHolder | undefined = undefined;
+  private _isFaceUp: boolean = false;
 
   onInserted: MulticastDelegate<
     (
@@ -43,53 +41,89 @@ export class MockCard extends MockGameObject implements Card {
     (card: this, removedCard: Card, position: number, player: Player) => void
   >();
 
-  takeCards(
-    numCards?: number | undefined,
-    fromFront?: boolean | undefined,
+  constructor(params?: MockCardParams) {
+    super(params);
+    if (params?.cardDetails) {
+      this._cardDetails.push(...params.cardDetails);
+    }
+    if (params?.cardHolder) {
+      this._cardHolder = params.cardHolder;
+    }
+    if (params?.isFaceUp !== undefined) {
+      this._isFaceUp = params.isFaceUp;
+    }
+  }
+
+  addCards(
+    cards: Card,
+    toFront?: boolean | undefined,
     offset?: number | undefined,
-    keep?: boolean | undefined
-  ): Card | undefined {
-    throw new Error("Method not implemented.");
+    animate?: boolean | undefined,
+    flipped?: boolean | undefined
+  ): boolean {
+    if (!this.canAddCards(cards)) {
+      return false;
+    }
+
+    const incoming = cards.getAllCardDetails().map(
+      (details) =>
+        new MockCardDetails({
+          index: details.index,
+          stackIndex: details.stackIndex,
+          templateId: details.templateId,
+          name: details.name,
+          metadata: details.metadata,
+          textureOverrideURL: details.textureOverrideURL,
+          flipped, // apply flipped!
+          tags: details.tags,
+        })
+    );
+    cards.destroy();
+
+    let index = this._cardDetails.length;
+    if (toFront) {
+      index = 0;
+    }
+    if (offset) {
+      if (toFront) {
+        index += offset;
+      } else {
+        index -= offset;
+      }
+    }
+    this._cardDetails = [
+      ...this._cardDetails.slice(0, index),
+      ...incoming,
+      ...this._cardDetails.slice(index),
+    ];
+    return true;
   }
-  split(numStacks: number): Card[] {
-    throw new Error("Method not implemented.");
+
+  canAddCards(cards: Card): boolean {
+    const mySize = this.getSize();
+    const inSize = cards.getSize();
+    const dx = Math.abs(mySize.x - inSize.x);
+    const dy = Math.abs(mySize.y - inSize.y);
+    return dx + dy < 0.1;
   }
-  shuffle(): void {
-    throw new Error("Method not implemented.");
+
+  divide(numCards: number): Card[] {
+    const numStacks = Math.ceil(this.getStackSize() / numCards);
+    const result: Card[] = [];
+    for (let i = 0; i < numStacks; i++) {
+      let newCard = this.takeCards(numCards);
+      if (!newCard) {
+        newCard = this; // one card remaining
+      }
+      result.push(newCard);
+    }
+    return result;
   }
-  setTextureOverrideURLAt(url: string, index?: number | undefined): void {
-    throw new Error("Method not implemented.");
+
+  getAllCardDetails(): CardDetails[] {
+    return this._cardDetails;
   }
-  setTextureOverrideURL(url: string): void {
-    throw new Error("Method not implemented.");
-  }
-  setInheritScript(inherit: boolean): void {
-    throw new Error("Method not implemented.");
-  }
-  setCardIndexAt(cardIndex: number, index?: number | undefined): void {
-    throw new Error("Method not implemented.");
-  }
-  removeFromHolder(): void {
-    throw new Error("Method not implemented.");
-  }
-  moveCardInStack(from: number, to: number): void {
-    throw new Error("Method not implemented.");
-  }
-  isInHolder(): boolean {
-    throw new Error("Method not implemented.");
-  }
-  isInHand(): boolean {
-    throw new Error("Method not implemented.");
-  }
-  isFaceUp(): boolean {
-    throw new Error("Method not implemented.");
-  }
-  getStackSize(): number {
-    return this._cardDetails.length;
-  }
-  getHolder(): CardHolder | undefined {
-    throw new Error("Method not implemented.");
-  }
+
   getCardDetails(index: number): CardDetails | undefined;
   getCardDetails(): CardDetails;
   getCardDetails(index?: unknown): CardDetails | undefined {
@@ -98,30 +132,155 @@ export class MockCard extends MockGameObject implements Card {
     }
     return this._cardDetails[0];
   }
-  getAllCardDetails(): CardDetails[] {
-    return this._cardDetails;
+
+  getHolder(): CardHolder | undefined {
+    return this._cardHolder;
   }
-  divide(numCards: number): Card[] {
-    throw new Error("Method not implemented.");
+
+  getStackSize(): number {
+    return this._cardDetails.length;
   }
+
+  isFaceUp(): boolean {
+    return this._isFaceUp;
+  }
+
+  isInHand(): boolean {
+    return this._cardHolder &&
+      this._cardHolder?.getOwningPlayer()?.getHandHolder() === this._cardHolder
+      ? true
+      : false;
+  }
+
+  isInHolder(): boolean {
+    return this._cardHolder ? true : false;
+  }
+
+  moveCardInStack(from: number, to: number): void {
+    const tmp = this._cardDetails[to];
+    this._cardDetails[to] = this._cardDetails[from];
+    this._cardDetails[from] = tmp;
+  }
+
+  removeFromHolder(): void {
+    this._cardHolder = undefined;
+  }
+
+  setCardIndexAt(cardIndex: number, index?: number | undefined): void {
+    index = index ? index : 0;
+    const details = this._cardDetails[index];
+    if (!details) {
+      return; // nothing to overwrite
+    }
+    this._cardDetails[index] = new MockCardDetails({
+      index: cardIndex,
+      stackIndex: details.stackIndex,
+      templateId: details.templateId,
+      name: details.name,
+      metadata: details.metadata,
+      textureOverrideURL: details.textureOverrideURL,
+      flipped: details.flipped,
+      tags: details.tags,
+    });
+  }
+
+  setInheritScript(inherit: boolean): void {}
+
+  setTextureOverrideURL(url: string): void {
+    this._cardDetails = this._cardDetails.map(
+      (details) =>
+        new MockCardDetails({
+          index: details.index,
+          stackIndex: details.stackIndex,
+          templateId: details.templateId,
+          name: details.name,
+          metadata: details.metadata,
+          textureOverrideURL: url,
+          flipped: details.flipped,
+          tags: details.tags,
+        })
+    );
+  }
+
+  setTextureOverrideURLAt(url: string, index?: number | undefined): void {
+    index = index ? index : 0;
+    const details = this._cardDetails[index];
+    if (!details) {
+      return; // nothing to overwrite
+    }
+    this._cardDetails[index] = new MockCardDetails({
+      index: details.index,
+      stackIndex: details.stackIndex,
+      templateId: details.templateId,
+      name: details.name,
+      metadata: details.metadata,
+      textureOverrideURL: url,
+      flipped: details.flipped,
+      tags: details.tags,
+    });
+  }
+
+  shuffle(): void {
+    const a: CardDetails[] = this._cardDetails;
+    // Fisher-Yates
+    for (let i = a.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+  }
+
+  split(numStacks: number): Card[] {
+    const stackSize = Math.ceil(this.getStackSize() / numStacks);
+    const result: Card[] = [];
+    for (let i = 0; i < numStacks; i++) {
+      let newCard = this.takeCards(stackSize);
+      if (!newCard) {
+        newCard = this; // one card remaining
+      }
+      result.push(newCard);
+    }
+    return result;
+  }
+
+  takeCards(
+    numCards?: number | undefined,
+    fromFront?: boolean | undefined,
+    offset?: number | undefined,
+    keep?: boolean | undefined
+  ): Card | undefined {
+    if (this._cardDetails.length === 1) {
+      return undefined; // "Returns undefined if this object is only a single card"
+    }
+
+    numCards = numCards !== undefined ? numCards : 1;
+    offset = offset !== undefined ? offset : 0;
+
+    let index = fromFront ? 0 : this._cardDetails.length - numCards;
+    if (offset) {
+      if (fromFront) {
+        index += offset;
+      } else {
+        index -= offset;
+      }
+    }
+    let cardDetails: CardDetails[];
+
+    if (keep) {
+      cardDetails = this._cardDetails.slice(index, index + numCards);
+    } else {
+      cardDetails = this._cardDetails.splice(index, numCards);
+    }
+    return new MockCard({ cardDetails });
+  }
+
+  // ----------------------------------
+
   deal(
     count?: number | undefined,
     slots?: number[] | undefined,
     faceDown?: boolean | undefined,
     dealToAllHolders?: boolean | undefined
   ): void {
-    throw new Error("Method not implemented.");
-  }
-  canAddCards(cards: Card): boolean {
-    throw new Error("Method not implemented.");
-  }
-  addCards(
-    cards: Card,
-    toFront?: boolean | undefined,
-    offset?: number | undefined,
-    animate?: boolean | undefined,
-    flipped?: boolean | undefined
-  ): boolean {
     throw new Error("Method not implemented.");
   }
 }
