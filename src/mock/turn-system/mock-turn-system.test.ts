2670f013b05876e3151a856ca9735ab07aec346b
Darrell <darrell.anderson@gmail.com>
2023-11-24 18:01:04 -0800

    progress

--- src/mock/turn-system/mock-turn-system.test.ts
+++ src/mock/turn-system/mock-turn-system.test.ts
@@ -1,5 +1,21 @@
-import { MockTurnSystem } from "./mock-turn-system";
+import { MockPhaseDetails } from "../phase-details/mock-phase-details";
+import { MockPlayer } from "../player/mock-player";
+import { MockTurnSystem, MockTurnSystemParams } from "./mock-turn-system";
 
 it("constructor", () => {
-  new MockTurnSystem();
+  const phase = new MockPhaseDetails();
+  const params: MockTurnSystemParams = {
+    phases: [phase],
+    currentTurn: 1,
+    currentRound: 2,
+    currentPhaseIndex: 0,
+    activePlayers: [new MockPlayer()],
+  };
+  const turnSystem = new MockTurnSystem(params);
+  expect(turnSystem.getAllPhases()).toEqual(params.phases);
+  expect(turnSystem.getActivePlayers()).toEqual(params.activePlayers);
+  expect(turnSystem.getCurrentPhase()).toEqual(phase);
+  expect(turnSystem.getCurrentPhaseIndex()).toEqual(params.currentPhaseIndex);
+  expect(turnSystem.getCurrentRound()).toEqual(params.currentRound);
+  expect(turnSystem.getCurrentTurn()).toEqual(params.currentTurn);
 });
--- src/mock/turn-system/mock-turn-system.ts
+++ src/mock/turn-system/mock-turn-system.ts
@@ -6,24 +6,56 @@ import {
 } from "@tabletop-playground/api";
 import { MockMulticastDelegate } from "../multicast-delegate/mock-multicast-delegate";
 
+export type MockTurnSystemParams = {
+  phases?: PhaseDetails[];
+  currentTurn?: number;
+  currentRound?: number;
+  currentPhaseIndex?: number;
+  activePlayers?: Player[];
+};
+
 export class MockTurnSystem implements TurnSystem {
+  private _phases: PhaseDetails[] = [];
+  private _currentTurn: number = 0;
+  private _currentRound: number = 0;
+  private _currentPhaseIndex: number = 0;
+  private _activePlayers: Player[] = [];
+
   onTurnChanged: MulticastDelegate<
     (previousTurn: number, previousPhase: number, previousRound: number) => void
   > = new MockMulticastDelegate<
     (previousTurn: number, previousPhase: number, previousRound: number) => void
   >();
 
+  constructor(params?: MockTurnSystemParams) {
+    if (params?.phases) {
+      this._phases = params.phases;
+    }
+    if (params?.currentTurn !== undefined) {
+      this._currentTurn = params.currentTurn;
+    }
+    if (params?.currentRound !== undefined) {
+      this._currentRound = params.currentRound;
+    }
+    if (params?.currentPhaseIndex !== undefined) {
+      this._currentPhaseIndex = params.currentPhaseIndex;
+    }
+    if (params?.activePlayers !== undefined) {
+      this._activePlayers = params.activePlayers;
+    }
+  }
+
   setPhases(phases: PhaseDetails[]): void {
-    throw new Error("Method not implemented.");
+    this._phases = phases;
   }
   setCurrentTurn(turn: number): void {
-    throw new Error("Method not implemented.");
+    this._currentTurn = turn;
   }
   setCurrentRound(round: number): void {
-    throw new Error("Method not implemented.");
+    this._currentRound = round;
   }
   setCurrentPhaseIndex(phase: number): void {
-    throw new Error("Method not implemented.");
+    this._currentPhaseIndex = phase;
   }
   previousTurn(): void {
     throw new Error("Method not implemented.");
@@ -32,21 +64,21 @@ export class MockTurnSystem implements TurnSystem {
     throw new Error("Method not implemented.");
   }
   getCurrentTurn(): number {
-    throw new Error("Method not implemented.");
+    return this._currentTurn;
   }
   getCurrentRound(): number {
-    throw new Error("Method not implemented.");
+    return this._currentRound;
   }
   getCurrentPhaseIndex(): number {
-    throw new Error("Method not implemented.");
+    return this._currentPhaseIndex;
   }
   getCurrentPhase(): PhaseDetails {
-    throw new Error("Method not implemented.");
+    return this._phases[this._currentPhaseIndex];
   }
   getAllPhases(): PhaseDetails[] {
-    throw new Error("Method not implemented.");
+    return this._phases;
   }
   getActivePlayers(): Player[] {
-    throw new Error("Method not implemented.");
+    return this._activePlayers;
   }
 }
